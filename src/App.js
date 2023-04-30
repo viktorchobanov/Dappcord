9c645a983c977a0b087f8f62111453988ebb8ffc
Viktor <viktor.chobanov10100@gmail.com>
2023-04-30 16:08:39 +0300

    Frontend boilerplate

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+import { io } from "socket.io-client"
+
+// Components
+import Navigation from './components/Navigation'
+import Servers from './components/Servers'
+import Channels from './components/Channels'
+import Messages from './components/Messages'
+
+// ABIs
+import Dappcord from './abis/Dappcord.json'
+
+// Config
+import config from './config.json';
+
+// Socket
+const socket = io('ws://localhost:3030');
+
+function App() {
+  const [account, setAccount] = useState(null)
+  const [provider, setProvider] = useState(null)
+  const [dappcord, setDappcord] = useState(null)
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+    const dappcord = new ethers.Contract(config[network.chainId].Dappcord.address, Dappcord, provider)
+
+    setDappcord(dappcord)
+
+    const channel = await dappcord.getChannel(1)
+
+    // Connect again when changing the account in MetaMask
+    window.ethereum.on('accountsChanged', async () => {
+      window.location.reload()
+    })
+
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
+
+
+  return (
+    <div>
+      <Navigation account={account} setAccount={setAccount} />
+      <main>
+
+        <Servers />
+        <Channels />
+        <Messages />
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
