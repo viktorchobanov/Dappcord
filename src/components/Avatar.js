2707f6d823280e2c425f9d400248530a1a9f97fc
Viktor <viktor.chobanov10100@gmail.com>
2023-08-30 13:25:54 +0300

    Metamask Avatar changed

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import { MetaMaskAvatar } from 'react-metamask-avatar';
+import blockies from 'blockies';
+
+const MetamaskAvatar = ({ address }) => {
+    const options = {
+        seed: address.toLowerCase(), // Use the Ethereum address as the seed
+        size: 10, // Set the size of the avatar
+        scale: 3, // Scale factor
+    };
+
+    const avatarDataURL = blockies(options).toDataURL();
+
+    return (
+        <img
+            src={avatarDataURL}
+            alt={`Metamask Avatar for ${address}`}
+            width={options.size * options.scale}
+            height={options.size * options.scale}
+        />
+    );
+};
 
 export const Avatar = ({ address }) => {
     return (
-        <MetaMaskAvatar address={address} size={24} />
+        <MetamaskAvatar address={address} size={24} />
     )
 }
