46e6fc4af1165dab3679b701489e1ece835052ea
Viktor <viktor.chobanov10100@gmail.com>
2023-08-30 12:59:59 +0300

    Deploy changes

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 const PORT = process.env.PORT || 3030
 const server = app.listen(PORT, () => console.log(`Listening on ${PORT}\n`))
@@ -54,9 +61,9 @@ const messages = [
 
 const { Server } = require("socket.io");
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000"
-    }
+    // cors: {
+    //     origin: "http://localhost:3000"
+    // } // needed for development purposes
 })
 
 io.on('connection', (socket) => {
